269539400abe6e2093b5c41a4d9be6edcc8db070
Vikas Solanki <solankivikas066@gmail.com>
2025-06-27 18:44:09 +0530

    first commit

---
+++ client/src/components/SectionList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SectionList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SectionList
---
+++ client/src/pages/Bookmarked.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bookmarked = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Bookmarked
