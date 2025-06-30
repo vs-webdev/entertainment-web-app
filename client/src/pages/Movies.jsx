77babd32cad66d36c5bdf451fd0a7b6816c70b4d
Vikas Solanki <solankivikas066@gmail.com>
2025-06-30 12:02:44 +0530

    Add Movies Page

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import MediaCard from "../components/MediaCard";
+import Search from "../components/Search"
 
-const Movies = () => {
+const Movies = ({movies, toggleBookmark}) => {
+  const handleSearch = (e) => null;
+
   return (
-    <div>
-      
+    <div className="h-full w-full">
+      <Search 
+        placeholder={"Search for movies or TV series"}
+        handleSearch={handleSearch}
+      />
+
+      <div className="flex flex-col items-start">
+        <h2 className="text-3xl mb-7">
+          Movies
+        </h2>
+        <ul className="grid grid-cols-[repeat(auto-fit,_minmax(318px,_1fr))] gap-8 w-full">
+          {movies.filter(movie => movie.category === 'Movie').map((movie, index) => (
+            <li key={index}>
+              <MediaCard
+                title={movie.title}
+                year={movie.year}
+                category={movie.category}
+                rating={movie.rating}
+                isBookmarked={movie.isBookmarked}
+                toggleBookmark={toggleBookmark}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
